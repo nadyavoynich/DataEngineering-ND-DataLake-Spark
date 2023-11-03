385817ea98682a87453b47b26e5c5a23b1ab28a3
Nadya Voynich <nadya.voynich@gmail.com>
2023-11-03 11:29:22 +0100

    Sanitized the Customer data (trusted zone)

---
+++ customer_landing_to_trusted.py
@@ -0,0 +1,60 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue import DynamicFrame
+
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Landing Customer Zone
+LandingCustomerZone_node1698089543781 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://whiterose-lake-house/customer/landing/"],
+        "recurse": True,
+    },
+    transformation_ctx="LandingCustomerZone_node1698089543781",
+)
+
+# Script generated for node SQL Query
+SqlQuery1355 = """
+select * from myDataSource
+where sharewithresearchasofdate is not null;
+"""
+SQLQuery_node1699004829590 = sparkSqlQuery(
+    glueContext,
+    query=SqlQuery1355,
+    mapping={"myDataSource": LandingCustomerZone_node1698089543781},
+    transformation_ctx="SQLQuery_node1699004829590",
+)
+
+# Script generated for node Trusted Customer Zone
+TrustedCustomerZone_node1698089945465 = glueContext.write_dynamic_frame.from_options(
+    frame=SQLQuery_node1699004829590,
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "path": "s3://whiterose-lake-house/customer/trusted/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="TrustedCustomerZone_node1698089945465",
+)
+
+job.commit()
